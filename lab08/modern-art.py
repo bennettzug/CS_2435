f9860ca9713804e04cba7955c8043f7a9d72fe9a
bennettzug <42450925+bennettzug@users.noreply.github.com>
2022-11-15 00:36:55 -0500

    Update modern-art.py

--- lab08/modern-art.py
+++ lab08/modern-art.py
@@ -5,6 +5,7 @@ from typing import Tuple, Callable, Generator
 from collections import namedtuple
 import random
 # commmenting this out for now, as importing the module took like .25 secs
+# might have thrown an error if it's run on a system where it isn't installed
 # import drawBot as messedupdrawbot
 # pretty sure something is wrong with the drawbot module.
 # this workaround 'fixes' it
