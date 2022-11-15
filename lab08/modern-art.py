d329eb1fd0269b9b333013aa2dbc4417099bdbc1
bennettzug <42450925+bennettzug@users.noreply.github.com>
2022-11-15 00:09:02 -0500

    made lab08 more performant

--- lab08/modern-art.py
+++ lab08/modern-art.py
@@ -1,6 +1,7 @@
 '''Solves modern art problem from lab 08'''
 import math
-from typing import Tuple, Callable
+from functools import lru_cache
+from typing import Tuple, Callable, Generator
 from collections import namedtuple
 import random
 # commmenting this out for now, as importing the module took like .25 secs
@@ -33,7 +34,7 @@ class Artwork:
     def __init__(self, filename) -> None:
         self.filename = filename
 
-    def read_file(self):
+    def read_file(self) -> Generator[Tuple, None, None]:
         'generator that yields each line of the file as the correct type'
         artwork_list = []
         with open(self.filename, encoding='utf-8') as artwork_file:
@@ -43,6 +44,7 @@ class Artwork:
         for item in artwork_list[1:]:
             yield ArtworkCircle(*item.split())
 
+    @lru_cache(maxsize=1000)
     def artwork(self) -> Tuple[ArtworkBackground, Tuple[ArtworkCircle, ...]]:
         '''returns a tuple of the form (background, (circle0...circleN))'''
         gen = self.read_file()
@@ -80,14 +82,14 @@ class Artwork:
     #     drawBot.saveImage(output_filename)
     #     drawBot.endDrawing()
 
-    def random_x_y(self):
+    def random_x_y(self) -> Tuple:
         '''Returns a tuple containing a random point within the canvas.'''
         rand_x = random.random() * float(self.artwork()[0].canvas_x)
         rand_y = random.random() * float(self.artwork()[0].canvas_y)
         return (rand_x, rand_y)
 
     # pylint: disable-next=invalid-name
-    def is_overlapping(self, x, y):
+    def is_overlapping(self, x, y) -> bool:
         '''Returns a boolean value as to whether or not a point
         is inside of one of the circles in the artwork.'''
         for circle in self.artwork()[1]:
@@ -99,7 +101,7 @@ class Artwork:
                 return False
         return True
 
-    def overlapping_test(self):
+    def overlapping_test(self) -> bool:
         '''Runs is_overlapping on a random point in the canvas.'''
         return self.is_overlapping(*self.random_x_y())
 
@@ -117,7 +119,7 @@ def bool_repeater(trials: int, function: Callable, success=True) -> float:
 def main() -> None:
     '''does all the things, should be pretty readable.'''
     artwork = Artwork('art.txt')
-    print(bool_repeater(10_000, artwork.overlapping_test))
+    print(bool_repeater(100_000, artwork.overlapping_test))
 
 
 if __name__ == '__main__':
