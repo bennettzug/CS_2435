e64280a25dbef44fa403c57567365d9dfdc99a0a
bennettzug <42450925+bennettzug@users.noreply.github.com>
2022-11-28 13:19:31 -0500

    cleaned up tictactoe

--- final/tictactoe.py
+++ final/tictactoe.py
@@ -1,15 +1,15 @@
 # take in a board state
-# determine if x or o won, if the game is ongoing, if the game resulted in a tie, or if it is an invalid board state
-o = open("state.txt", 'r')
-board = []
-for line in o:
-    board.append(line.rstrip().lower())
-o.close()
+# determine if x or o won, if the game is ongoing, or if the game resulted in a tie
+with open("state.txt") as o:
+    board = []
+    for line in o:
+        board.append(line.rstrip().lower())
+
 # count number of each type
 def letterCounter(board):
-    x=0
-    o=0
-    empty=0
+    x = 0
+    o = 0
+    empty = 0
     for i in board:
         for ch in i:
             if ch == 'x':
@@ -22,23 +22,21 @@ def letterCounter(board):
 
 
 x, o, empty = letterCounter(board)
-# check validity (wrong number of pieces/player)
-
 # check x winning
 if 'xxx' in board \
     or (board[0][0] + board[1][0] + board[2][0] == 'xxx')\
-         or (board[0][1] + board[1][1] + board[2][1] == 'xxx')\
-             or (board[0][2] + board[1][2] + board[2][2] == 'xxx')\
-                 or (board[0][0] + board[1][1] + board[2][2] == 'xxx')\
-                     or (board[0][2] + board[1][1] + board[2][0] == 'xxx'):
+    or (board[0][1] + board[1][1] + board[2][1] == 'xxx')\
+    or (board[0][2] + board[1][2] + board[2][2] == 'xxx')\
+    or (board[0][0] + board[1][1] + board[2][2] == 'xxx')\
+        or (board[0][2] + board[1][1] + board[2][0] == 'xxx'):
     print("X wins")
 # check o winning
 elif 'ooo' in board \
     or (board[0][0] + board[1][0] + board[2][0] == 'ooo')\
-         or (board[0][1] + board[1][1] + board[2][1] == 'ooo')\
-             or (board[0][2] + board[1][2] + board[2][2] == 'ooo')\
-                 or (board[0][0] + board[1][1] + board[2][2] == 'ooo')\
-                     or (board[0][2] + board[1][1] + board[2][0] == 'ooo'):
+    or (board[0][1] + board[1][1] + board[2][1] == 'ooo')\
+    or (board[0][2] + board[1][2] + board[2][2] == 'ooo')\
+    or (board[0][0] + board[1][1] + board[2][2] == 'ooo')\
+        or (board[0][2] + board[1][1] + board[2][0] == 'ooo'):
     print('O wins')
 # check tie-ness
 elif empty == 0:
