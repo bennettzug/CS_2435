27bb451032ddcbbbd2493ad1efa1bab6a6b9b7a1
bennettzug <42450925+bennettzug@users.noreply.github.com>
2022-11-28 13:13:05 -0500

    set up final files

--- final/tictactoe.py
+++ final/tictactoe.py
@@ -1,11 +1,10 @@
 # take in a board state
 # determine if x or o won, if the game is ongoing, if the game resulted in a tie, or if it is an invalid board state
-o = open("tic.txt", 'r')
+o = open("state.txt", 'r')
 board = []
 for line in o:
     board.append(line.rstrip().lower())
 o.close()
-print(board)
 # count number of each type
 def letterCounter(board):
     x=0
@@ -21,18 +20,18 @@ def letterCounter(board):
                 empty += 1
     return x, o, empty
 
-x,o,empty = letterCounter(board)
-#check validity (wrong number of pieces/player)
-if not -1 < x-o < 1:
-    print("games invalid")
+
+x, o, empty = letterCounter(board)
+# check validity (wrong number of pieces/player)
+
 # check x winning
-elif 'xxx' in board \
+if 'xxx' in board \
     or (board[0][0] + board[1][0] + board[2][0] == 'xxx')\
          or (board[0][1] + board[1][1] + board[2][1] == 'xxx')\
              or (board[0][2] + board[1][2] + board[2][2] == 'xxx')\
                  or (board[0][0] + board[1][1] + board[2][2] == 'xxx')\
                      or (board[0][2] + board[1][1] + board[2][0] == 'xxx'):
-    print("X Won")
+    print("X wins")
 # check o winning
 elif 'ooo' in board \
     or (board[0][0] + board[1][0] + board[2][0] == 'ooo')\
@@ -40,10 +39,10 @@ elif 'ooo' in board \
              or (board[0][2] + board[1][2] + board[2][2] == 'ooo')\
                  or (board[0][0] + board[1][1] + board[2][2] == 'ooo')\
                      or (board[0][2] + board[1][1] + board[2][0] == 'ooo'):
-    print('O Won')
+    print('O wins')
 # check tie-ness
 elif empty == 0:
-    print('game is a tie')
+    print('Tie')
 # else game must be ongoing
 else:
-    print('games ongoing')
+    print('Incomplete')
